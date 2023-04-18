0ee864318f4e0f886d1fd710229dd1132d50a9bc
shreyailango <42718426+shreyailango@users.noreply.github.com>
2023-04-18 12:35:00 -0500

    1 file modified

--- customFetch.js
+++ customFetch.js
@@ -1,59 +1,34 @@
-/*export const Fetch = (url, options = {}) => {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    let method = 'GET' || options.method;
-    xhr.open(method, url);
-    if (options.headers) {
-      Object.keys(options.headers).forEach((key) => {
-        xhr.setRequestHeader(key, options.headers[key]);
-      });
-    }
-    xhr.onload = function () {
-      if (this.status >= 200 && this.status < 300) {
-        resolve({
-          json: () => Promise.resolve(JSON.parse(this.response)),
-          text: () => Promise.resolve(this.response),
-        });
-      } else {
-        reject({
-          status: this.status,
-          statusText: xhr.statusText,
-        });
-      }
-    };
-    xhr.onerror = function () {
-      reject({
-        status: this.status,
-        statusText: xhr.statusText,
-      });
-    };
-    xhr.send(options.body);
-  });
-}; */
-
 export const Fetch = (url, options = {}) => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     let method = 'GET' || options.method;
     xhr.open(method, url);
     if (options.headers) {
+      let keys = Object.keys(options.headers);
       Object.keys(options.headers).forEach((key) => {
         xhr.setRequestHeader(key, options.headers[key]);
       });
     }
+    // Setup our listener to process compeleted requests
+    xhr.onload = () => {
+      // Only run if the request is complete
+      if (xhr.readyState !== 4) return;
 
-    function onLoadListener() {
-      var data = JSON.parse(this.responseText);
-      console.log(data);
-    }
-
-    function onErrorListener(err) {
-      console.log('XHR Error :', err);
-    }
-
-    request.onload = onLoadListener;
-    request.onerror = onErrorListener;
-    request.open('get', 'url', true);
-    request.send();
+      // Process our return data
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve({
+          json: () => Promise.resolve(JSON.parse(this.response)),
+          text: () => Promise.resolve(this.response),
+        });
+      } else {
+        // What to do when the request has failed
+        reject({
+          status: this.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+    //xhr.open(method, url);
+    xhr.send(options.body);
   });
 };
